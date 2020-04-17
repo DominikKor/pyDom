4367940f0609d4711917b0baabe2c00312aa5591
Jan <janekk@hotmail.com>
2020-04-17 19:52:41 +0200

    email correction

--- app.py
+++ app.py
@@ -74,4 +74,3 @@ def not_found(e):
 
 if __name__ == "__main__":
     app.run()
-
--- templates/mailer.php
+++ templates/mailer.php
@@ -22,7 +22,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contact@markups.io";
+        $recipient = "droko@gmx.de";
 
         // Set the email subject.
         $subject = "New contact from $name";

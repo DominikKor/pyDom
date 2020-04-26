750f2b2a5c889585fa0f76d3c9d49370d7ee19e8
Jan <janekk@hotmail.com>
2020-04-26 22:21:46 +0200

    age.py working - visitor count removed

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
+Jahr = 2020
+Monat = 4
+Tag = 26
+
 @app.route("/")
 def redirectindex():
     return redirect(url_for("index"))
@@ -22,7 +26,66 @@ def age():
 
 @app.route("/agecalculation/")
 def agecalculation():
-    return render_template("agecalculation.html", birthyear=birthyear)
+    birthyearSplitted = birthyear.split("-")
+    InputTag = birthyearSplitted[2]
+    InputMonat  = birthyearSplitted[1]
+    InputJahr = birthyearSplitted[0]
+    # remergedBirthyear = f"{splitted_day}/{splitted_month}/{splitted_year}"
+
+    Antwort = None
+    AntwortWurdest = "Du wurdest"
+    GeburtstagAntwortTeilEins = "Alles Gute zu deinem"
+    GeburtstagAntwortTeilZwei = "Geburtstag!"
+    AntwortWirst = "Du wirst"
+    Geburtstag = "nein"
+    GanzeAntwort = None
+
+    Alter = Jahr - int(InputJahr)
+
+    if Alter == 0:
+        Alter = "geboren"
+
+    InputJahrAlsInt = int(InputJahr)
+    InputMonatAlsInt = int(InputMonat)
+    InputTagAlsInt = int(InputTag)
+
+    if InputMonatAlsInt > Monat:
+        Antwort = AntwortWirst
+        GanzeAntwortEins = f"{Antwort} in diesem Jahr {str(Alter)}. "
+        GanzeAntwortZwei = f"Jetzt bist du aber {Alter - 1} Jahre alt."
+        GanzeAntwort = f"{GanzeAntwortEins}{GanzeAntwortZwei}"
+    elif InputMonatAlsInt < Monat:
+        Antwort = AntwortWurdest
+        if Alter == "geboren":
+            GanzeAntwort = f"{Antwort} in diesem Jahr {str(Alter)}!"
+        else:
+            GanzeAntwort = f"{Antwort} in diesem Jahr {str(Alter)}."
+    else:
+        if InputTagAlsInt > Tag:
+            Antwort = AntwortWurdest
+        else:
+            if InputTagAlsInt == Tag:
+                Geburtstag = "ja"
+                Antwort = GeburtstagAntwortTeilEins
+            else:
+                Antwort = AntwortWurdest
+        if Geburtstag == "ja":
+            if (InputJahrAlsInt == Jahr) and (InputMonatAlsInt == Monat) and (InputTagAlsInt == Tag):
+                GanzeAntwort = "Alles Gute zur Geburt!"
+            else:
+                GanzeAntwort = f"{GeburtstagAntwortTeilEins} {Alter} {GeburtstagAntwortTeilZwei}"
+        else:
+            if Alter == "geboren":
+                GanzeAntwort = f"{Antwort} in diesem Jahr {str(Alter)}!"
+            else:
+                if Antwort == AntwortWirst:
+                    GanzeAntwortEins = f"{Antwort} in diesem Jahr {str(Alter)}. "
+                    GanzeAntwortZwei =  f"Jetzt bist du aber {Alter - 1} Jahr alt."
+                    GanzeAntwort = f"{GanzeAntwortEins}{GanzeAntwortZwei}"
+                else:
+                    GanzeAntwort = f"{Antwort} in diesem Jahr {str(Alter)}."
+    return GanzeAntwort
+    #return render_template("agecalculation.html", birthyear=birthyear)
 
 @app.route("/404.html/")
 def page404():

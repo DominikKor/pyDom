9659759db89465820b9535e686ca9b913e47ec4b
Jan <janekk@hotmail.com>
2020-05-12 21:16:31 +0200

    switch to postgresql database

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, current_user, logout_user, login_required
+from psycopg2 import *
 
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "87aa28f53d12b98ffe7f439aa7eaf268"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
-# "postgres://rckkgblinsrymr:37831f40b6effc3344ffb24b2dda03c8760fb0daeb820ce368361ab8d7290edc@ec2-54-165-36-134.compute-1.amazonaws.com:5432/dcu9gh8escihdu"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://rckkgblinsrymr:37831f40b6effc3344ffb24b2dda03c8760fb0daeb820ce368361ab8d7290edc@ec2-54-165-36-134.compute-1.amazonaws.com:5432/dcu9gh8escihdu"
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
